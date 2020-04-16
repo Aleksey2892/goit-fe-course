007612c218c8ba482e78aafddaf17db69584a1a1
Aleksey <aleksey2892@gmail.com>
2020-04-16 19:08:37 +0300

    ready ex 5

--- javascript/module-01/excercise-05/index.js
+++ javascript/module-01/excercise-05/index.js
@@ -1,35 +1,52 @@
 let cost;
 let delivery;
+let country;
 
 delivery = prompt('Укажите страну доставки');
-console.log(delivery.toLowerCase());
-delivery = delivery.toLowerCase();
+// console.log(delivery.toLowerCase());
+if (delivery === null) {
+  console.log('Отменено пользователем!');
+} else delivery = delivery.toLowerCase();
 
 switch (delivery) {
   case 'китай':
+    delivery = '"Китай"';
     cost = 100;
     break;
 
   case 'чили':
     cost = 250;
+    delivery = '"Чили"';
     break;
 
   case 'австралия':
     cost = 170;
+    delivery = '"Австралия"';
     break;
 
   case 'индия':
     cost = 80;
+    delivery = '"Индия"';
     break;
 
   case 'ямайка':
     cost = 120;
+    delivery = '"Ямайка"';
     break;
 
   default:
-    alert(
-      'В вашей стране доставка не доступна. Доставка возможна в: "Китай", "Чили", "Австралия", "Индия", "Ямайка"',
-    );
+    if (delivery !== null) {
+      alert(
+        'В вашей стране доставка не доступна. Доставка возможна в: "Китай", "Чили", "Австралия", "Индия", "Ямайка"',
+      );
+    }
 }
 
-console.log(cost`Доставка в ${delivery} будет стоить ${cost} креитов`);
+if (
+  delivery === '"Китай"' ||
+  delivery === '"Австралия"' ||
+  delivery === '"Индия"' ||
+  delivery === '"Ямайка"'
+) {
+  alert(`Доставка в ${delivery} будет стоить ${cost} кредитов`);
+}
