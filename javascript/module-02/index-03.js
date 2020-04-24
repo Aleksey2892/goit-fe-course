15d7f0c2cab456eee98203df6d7da52b7c1815fd
Aleksey <aleksey2892@gmail.com>
2020-04-24 18:17:08 +0300

    ready ex-3

---
+++ javascript/module-02/index-03.js
@@ -0,0 +1,18 @@
+const findLongestWord = function (string) {
+  const words = string.split(' ');
+  let numberLetter = 0;
+  let longestWord;
+
+  for (const word of words) {
+    if (word.length > numberLetter) {
+      numberLetter = word.length;
+      longestWord = word;
+    }
+  }
+
+  return `The longest word of string: '${longestWord}'`;
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('May the force be with you')); // 'force'
