4a7df1bbf13cc4816bf88e603b75cf4afbb309c2
Aleksey <aleksey2892@gmail.com>
2020-04-25 19:05:33 +0300

    ready ex-5

---
+++ javascript/module-02/index-05.js
@@ -0,0 +1,18 @@
+const checkForSpam = message => {
+  let string = message.toLowerCase();
+  let checkWord = false;
+
+  if (string.includes('spam') || string.includes('sale') === true) {
+    checkWord = true;
+  }
+
+  return checkWord;
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
