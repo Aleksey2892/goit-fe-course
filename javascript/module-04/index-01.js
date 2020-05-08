65e73357137aab1a12f08c01e1bb6e2e6b43a402
Aleksey <aleksey2892@gmail.com>
2020-05-09 02:08:34 +0300

    ready task 2

--- javascript/module-04/index-01.js
+++ javascript/module-04/index-01.js
@@ -4,22 +4,22 @@ const account = {
   discount: 0.1,
   orders: ['order-1', 'order-2', 'order-3'],
   changeDiscount(value) {
-    discount = value;
+    this.discount = value;
   },
   showOrders() {
-    return orders;
+    return this.orders;
   },
   addOrder(cost, order) {
-    balance -= cost;
-    orders.push(order);
+    this.balance -= cost;
+    this.orders.push(order);
   },
 };
 
-//   account.changeDiscount(0.15);
-//   console.log(account.discount); // 0.15
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-//   console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
 
-//   account.addOrder(5000, 'order-4');
-//   console.log(account.balance); // 19000
-//   console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
