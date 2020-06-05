2c74847a3882edd334b82b308874d63cd372c028
Aleksey <aleksey2892@gmail.com>
2020-06-05 20:19:57 +0300

    fix task 6 module 7

--- javascript/module-07/index-06.js
+++ javascript/module-07/index-06.js
@@ -5,21 +5,34 @@ const validDataLength = Number(inputTextRef.getAttribute('data-length'));
 // event
 inputTextRef.addEventListener('blur', inputTextHandler); // eslint-disable-line
 
-// fn
+// event handler
 function inputTextHandler() {
-  if (event.target.value.length < validDataLength) {
-    inputTextRef.classList.add('invalid');
-    if (inputTextRef.classList.contains('valid')) {
-      inputTextRef.classList.remove('valid');
-    }
-  } else {
-    inputTextRef.classList.add('valid');
-    if (inputTextRef.classList.contains('invalid')) {
-      inputTextRef.classList.remove('invalid');
-    }
-  }
+  event.target.value.length < validDataLength // eslint-disable-line
+    ? addInvalidClass() // eslint-disable-line
+    : addValidClass(); // eslint-disable-line
 
   if (!event.target.value.length) {
-    inputTextRef.classList.remove('valid', 'invalid');
+    removeClass(); // eslint-disable-line
   }
+}
+
+// fns
+function addValidClass() {
+  inputTextRef.classList.add('valid');
+  if (inputTextRef.classList.contains('invalid')) {
+    inputTextRef.classList.remove('invalid');
+  }
+}
+
+function addInvalidClass() {
+  inputTextRef.classList.add('invalid');
+  if (inputTextRef.classList.contains('valid')) {
+    inputTextRef.classList.remove('valid');
+  }
+}
+
+function removeClass() {
+  inputTextRef.classList.contains('valid') // eslint-disable-line
+    ? inputTextRef.classList.remove('valid') // eslint-disable-line
+    : inputTextRef.classList.remove('invalid'); // eslint-disable-line
 }
